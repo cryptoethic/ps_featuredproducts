c442d191e5ca36f121e78f4192b211c3a31aaaeb
Maxime Biloé <maxime.biloe@prestashop.com>
2016-10-27 17:39:54 +0200

    // Version updated to v1.0.6

--- ps_featuredproducts.php
+++ ps_featuredproducts.php
@@ -44,7 +44,7 @@ class Ps_FeaturedProducts extends Module implements WidgetInterface
     {
         $this->name = 'ps_featuredproducts';
         $this->tab = 'front_office_features';
-        $this->version = '1.0.5';
+        $this->version = '1.0.6';
         $this->author = 'PrestaShop';
         $this->need_instance = 0;
         $this->ps_versions_compliancy = [

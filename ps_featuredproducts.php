e51c4a1c07a309b7b6669fb85ed5467e7ac307f0
Progi1984 <franck.lefevre@prestashop.com>
2020-06-22 11:43:13 +0200

    Bump version to 2.1.0

--- ps_featuredproducts.php
+++ ps_featuredproducts.php
@@ -46,7 +46,7 @@ class Ps_FeaturedProducts extends Module implements WidgetInterface
     {
         $this->name = 'ps_featuredproducts';
         $this->author = 'PrestaShop';
-        $this->version = '2.0.0';
+        $this->version = '2.1.0';
         $this->need_instance = 0;
 
         $this->ps_versions_compliancy = [
